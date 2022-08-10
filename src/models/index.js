f1572f8563f0158d0e3c6df584605193d38c41d8
Agustin Rojas <rojasagusf01@gmail.com>
2022-08-10 17:33:30 -0300

    Add models

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+const Star = require('./star');
+const User = require('./user');
+const Constellation = require('./constellation');
+
+module.exports = {
+    Star,
+    User,
+    Constellation
+};
---
+++ src/models/star.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const {Schema} = require('mongoose');
+
+const starSchema = new Schema({
+    name: {
+        type: String
+    },
+    constellation: {
+        ref: 'Constellation',
+        type: Schema.Types.ObjectId
+    },
+    magnitude: {
+        type: String
+    },
+    distance: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Star', starSchema);
